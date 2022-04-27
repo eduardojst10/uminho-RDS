354d3f346ebe2ddb57beafb80cfdada2136de2ff
eduardojst10 <eduardojst99@gmail.com>
2022-04-27 19:59:49 +0100

    l3switch atualizado e incompleto

--- TP1/l3switch.py
+++ TP1/l3switch.py
@@ -117,7 +117,6 @@ class L3switch(app_manager.RyuApp):
             return
         
 
-
         #------------ ARP 
         pkt_arp = pkt.get_protocol(arp.arp)
         # Dado um Match com um ARP Request
@@ -142,68 +141,6 @@ class L3switch(app_manager.RyuApp):
             self.handle_ipv4(datapath,in_port,pkt_eth,pkt_ipv4)
             return 
         
-        
-
-
-        #------------ Controlo de dictionary de MAC src e port associada  ADIÇÃO DE FLOWS a ver mais à frente
-
-
-        # MACs dst e src
-        dst = pkt_eth.dst
-        src = pkt_eth.src
-        #id de switch
-        dpid = format(datapath.id, "d").zfill(16)
-        self.mac_to_port.setdefault(dpid, {})
-
-        # manutencao de logs
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
-
-        # Popular o dictionary com MACs
-        # Dictionary com o input packet’s source MAC Address and input port.
-        self.mac_to_port[dpid][src] = in_port
-
-   
-        # Then use that same dictionary and with the destination MAC address find out the packet’s destination
-        # Usar o mesmo dicionario com o MAC addr dst para descobrir a porta de destino
-        # Se é nula entao nao necessitamos de criar um of_flow_mod
-
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-
-        # O que o switch vai fazer é enviar a mensagem para o out_port
-        # OFPActionOutput class é usada com uma mensagem packet_out para especificar uma porta de switch para qual se envia o pacote. 
-        actions = [parser.OFPActionOutput(out_port)]
-
-      
-        # Adicionamos novo flow com timeout e cenas especificas a uma de tabelas de flows?
-
-
-        
-        # Submeter um novo flow de modo a evita packet_in na proxima vez
-        # Caso a porta de saida não seja nula, temosd de criar um novo of_flow_modobject com um match field
-        # (dst MAC address) and a action field (sent to ’destination port’)
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            
-            # Verificacao de buffer_id válido, if yes avoid to send both 
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        # classe OFPPacketOut é usada para construir um packet_out
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
 
 
     # Ver com o stor
@@ -321,24 +258,30 @@ class L3switch(app_manager.RyuApp):
         return
         
 
-    # Incompleto
+    # Incompleto provavelmente
     def handle_icmp(self, datapath, port, pkt_ethernet, pkt_ipv4, pkt_icmp):
         if pkt_icmp.type != icmp.ICMP_ECHO_REQUEST:
             return
 
         else:
-            #Match do ip do qual queremos o mac
-            ip_wanted = pkt_icmp.dst_ip
 
-            #vou buscar o mac pretendido
-            mac_wanted = self.ip_mac[ip_wanted]
+            # Com IP de host tenho dados sobre por onde vou dar forwarding do meu router/L3switch 
+            data_ipdst = self.interfaces_routing[pkt_ipv4.src]
+
+            # IP de interface do router/L3Switch
+            src_ip_interface = data_ipdst[0]
+
+            # MAC de interface do router/L3Switch
+            src_mac_interface = self.ip_mac[src_ip_interface]
+
+
 
             pkt = packet.Packet()
             pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
                                             dst=pkt_ethernet.src,
-                                            src=mac_wanted))
+                                            src=src_mac_interface))
             pkt.add_protocol(ipv4.ipv4(dst=pkt_ipv4.src,
-                                    src=self.ip_addr,
+                                    src=src_ip_interface,
                                     proto=pkt_ipv4.proto))
 
             pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY,
